b1b46458cf91e265d1a261febbb537593a129b83
GunnarBoll <gunnar.bollmark@physics.uu.se>
2019-01-21 12:06:58 +0100

    Update tMPS.py

--- tMPS.py
+++ tMPS.py
@@ -4,10 +4,8 @@ Written by: Gunnar Bollmark
 import numpy as np
 import time as t
 import scipy.sparse as sp
-import datetime
 import imp
-import pathlib
-import sys
+import os
 
 import storage as st
 import ExactDiag as ed
@@ -67,52 +65,13 @@ def obser_test(ED, op):
     print(corr_list)
     print("Expectation value of observable")
     print(maglist)
-    print("Time taken for measurement (ED):", t2-t1)
-
-def filewrite(Psi, H, ED, T, keylist):
-    date = str(datetime.date.today())
-    run_number = 1
-    direc = ("C:/Users/Gunnar/Documents/Ph.D/Learning/DMRG/Tryout code/output/"
-             + date)
-    while True:
-        try:
-            pathlib.Path(direc + "_" + str(run_number) + "/").mkdir(
-                parents=True, exist_ok=False)
-            break
-        except FileExistsError:
-            run_number += 1
-    direc = direc + "_" + str(run_number) + "/"
-    name = "chi=" + str(H.chi) + ",T=" + str(T) + ",dt=" + str(H.dt)
-    fw = open(direc+name+".txt", "x")
-    
-    adag = np.array([[0, 1], [0, 0]])
-    a = np.array([[0, 0], [1, 0]])
-    fw.write("# Algorithm data\n")
-    fw.write("# GS Energy\n")
-    algo_GSE = sum(Psi.get_ener(H.Hchain))
-    fw.write(str(algo_GSE) + "\n")
-    meas = st.Measure()
-    fw.write("# Correlators\n")
-    for ind1 in range(H.N):
-        for ind2 in range(H.N):
-            corr = meas.correl(Psi, adag, a, ind1, ind2)
-            fw.write(str(corr) + "\n")
-    fw.write("# ED data\n")
-    fw.write("# GS Energy\n")
-    fw.write(str(ED.E_GS) + "\n")
-    fw.write("# Correlators\n")
-    for ind1 in range(ED.N):
-        for ind2 in range(ED.N):
-            corr = ED.ED_correl(ED.GS, adag, a, ind1, ind2)
-            fw.write(str(corr) + "\n")
-    fw.close()
-    
+    print("Time taken for measurement (ED):", t2-t1)    
 
 # Main program
 def main():
     model = input("Enter model (Heisen, HCboson): ")
-    directory = ("C:/Users/Gunnar/Documents/Ph.D/Learning/DMRG/Tryout code/"
-                  + "models/" + model + ".txt")
+    directory = (os.path.dirname(os.path.realpath(__file__)) + "/" + model
+                 + ".txt")
     f = open(directory, "r")
     inputlist = []
     keylist = {}
@@ -181,6 +140,5 @@ def main():
         Free = st.FreeFerm(g1, g2, N)
         # print("\nFree fermion result:", Free.E_GS)
     
-    # filewrite(Psi, H, ED, T, keylist)
     return 0
 main()
