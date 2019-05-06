e74242a5df5d6b0ea86eb528a69e06b6d30916f6
GunnarBoll <gunnar.bollmark@physics.uu.se>
2019-05-06 17:03:21 +0200

    Update data_retrieval.py

--- data_retrieval.py
+++ data_retrieval.py
@@ -19,10 +19,8 @@ def get_plot_data(*args, **kwargs):
     meas_folder = "/proj/snic2019-8-26/measurements/"
     folder_order = ["tperp", "N", "n", "U", "chi"]
     
-    print(args)
     if args == ():
         args = sys.argv[1:]
-    print(args)
     
     obser = str(args[0])
     var_param = str(args[1])
