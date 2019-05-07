fdfb7bcdd07f30a3349383a101305f9bfd08ef6d
GunnarBoll <gunnar.bollmark@physics.uu.se>
2019-05-07 12:18:59 +0200

    Update data_retrieval.py

--- data_retrieval.py
+++ data_retrieval.py
@@ -55,11 +55,10 @@ def get_plot_data(*args, **kwargs):
         elif ind > num_dirs:
             break
         else:
-            if (obser + '.dat' not in contents[2] or
-                var_param + '.dat' not in contents[2]):
+            if obser + '.dat' not in contents[2]:
                 print(contents[0] + " does not contain the appropriate files "
-                      + obser + '.dat and ' + var_param + '.dat')
-                var_par_folder_contents.remove(contents[0])
+                      + obser + '.dat')
+                var_par_folder_contents.pop(ind-1)
     
     var_param_vals = [eval(var_par_str.replace(var_param+"=",''))
                       for var_par_str in var_par_folder_contents]
