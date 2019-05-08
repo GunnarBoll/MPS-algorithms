57713790bd541a3101aa8e74dc1a5d6953f25fb9
GunnarBoll <gunnar.bollmark@physics.uu.se>
2019-05-08 09:05:00 +0200

    Add treatment of inf

--- data_retrieval.py
+++ data_retrieval.py
@@ -70,7 +70,8 @@ def get_plot_data(*args, **kwargs):
     obser_vals = []
     if var_param == 'N':
         file_loc = (lambda p: meas_folder + Lp + 'N=' + str(p) + '/n='
-                    + str(int(p/2 + n)) + Rp)
+                    + str(int(p/2 + n)) + Rp if p != inf else 
+                    meas_folder + Lp + 'N=' + str(p) + '/n=' + str(n) + Rp)
     else:
         file_loc = (lambda p: meas_folder + Lp + folder_order[var_param_index] 
                     + '=' + str(p) + Rp)
