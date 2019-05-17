605b2fb22c1ce0db4d233d40ba6c2c0c859c67d3
GunnarBoll <gunnar.bollmark@physics.uu.se>
2019-05-17 17:02:29 +0200

    Update charge_gap_fill.py

--- charge_gap_fill.py
+++ charge_gap_fill.py
@@ -20,6 +20,7 @@ def charge_gap_fill(*args):
     U = eval(args[2])
     chi = eval(args[3])
     
+    print(args)
     energies, fillings = get_plot_data('Energy', 'n', *args)
     
     gap = energies[0] + energies[2] - 2*energies[1]
